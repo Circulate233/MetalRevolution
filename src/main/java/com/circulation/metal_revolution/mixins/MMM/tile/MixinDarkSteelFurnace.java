7b676093ba5da22f99d18b0cda4c9e15fbd46c5e
Circulate233 <a18966009058@163.com>
2025-10-13 20:53:17 +0800

    完成高炉，黑暗高炉，暗能量熔炉

---
+++ src/main/java/com/circulation/metal_revolution/mixins/MMM/tile/MixinDarkSteelFurnace.java
@@ -0,0 +1,42 @@
+package com.circulation.metal_revolution.mixins.MMM.tile;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+import project.studio.manametalmod.dark_magic.TileEntityDarkSteelFurnace;
+
+@Mixin(TileEntityDarkSteelFurnace.class)
+public class MixinDarkSteelFurnace {
+
+    @Unique
+    private static final int[] m$AllSlot = {0, 1};
+
+    /**
+     * @author circulation
+     * @reason 覆写
+     */
+    @Overwrite
+    public int[] getAccessibleSlotsFromSide(int side) {
+        return m$AllSlot;
+    }
+
+    /**
+     * @author circulation
+     * @reason 覆写
+     */
+    @Overwrite
+    public boolean canInsertItem(int slot, ItemStack stack, int side) {
+        return slot == 0 && FurnaceRecipes.smelting().getSmeltingResult(stack) != null;
+    }
+
+    /**
+     * @author circulation
+     * @reason 覆写
+     */
+    @Overwrite
+    public boolean canExtractItem(int slot, ItemStack stack, int side) {
+        return slot == 1;
+    }
+}
